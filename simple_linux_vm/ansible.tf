f3da9893276e09a44e300d0845bbfbcf672da57b
Your Name <you@example.com>
2024-03-05 18:28:02 +0100

    add ansible to the mix

--- simple_linux_vm/ansible.tf
+++ simple_linux_vm/ansible.tf
@@ -1,9 +1,39 @@
-resource "ansible_host" "my_ec2" {
+resource "ansible_host" "example" {
   name   = azurerm_public_ip.publicip.fqdn 
   groups = ["nginx"]
   variables = {
     ansible_user                 = var.adminUsername,
     ansible_ssh_private_key_file = "~/.ssh/id_rsa",
     ansible_python_interpreter   = "/usr/bin/python3",
+    ansible_ssh_common_args="-o StrictHostKeyChecking=no"
   }
+}
+
+resource "ansible_playbook" "example_2" {
+  depends_on = [ansible_host.example, azurerm_linux_virtual_machine.example]
+  playbook = "simple-playbook.yaml"
+
+  # inventory configuration
+  name   = azurerm_public_ip.publicip.fqdn 
+  
+  # connection configuration and other vars
+  extra_vars = {
+    inventory_file = "inventory.yaml"
+    ansible_hostname   = ansible_host.example.name
+    inventory_hostname = ansible_host.example.name
+    ansible_user = var.adminUsername
+    # injected_variable  = "Hello from simple.tf!"
+    ansible_check_mode = true
+  }
+  ignore_playbook_failure = true 
+  verbosity=3
+}
+
+output "playbook_stderr" {
+  value = ansible_playbook.example_2.ansible_playbook_stderr
+}
+
+# output stdout
+output "playbook_stdout" {
+    value = ansible_playbook.example_2.ansible_playbook_stdout
 }
--- simple_linux_vm/main.tf
+++ simple_linux_vm/main.tf
@@ -107,7 +107,7 @@ resource "azurerm_network_security_rule" "ssh" {
   destination_address_prefix  = "*"
 }
 
-resource "azurerm_network_security_rule" "rdp" {
+resource "azurerm_network_security_rule" "web" {
   name                        = "rdpIn"
   network_security_group_name = azurerm_network_security_group.example.name
   resource_group_name         = azurerm_resource_group.rg.name
@@ -116,7 +116,7 @@ resource "azurerm_network_security_rule" "rdp" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "3389"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
 }
